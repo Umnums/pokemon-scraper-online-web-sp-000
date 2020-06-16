e4b8e56556ee4a08342904f6c87ab6b442b97615
Joseph Kao <josephtkao@gmail.com>
2020-06-16 00:30:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,7 +20,7 @@ class Pokemon
     row = db.execute(sql, id)[0]
     hash = {:id => row[0], :name => row[1], :type => row[2], :db => db}
     # print row
-    print hash
+    # print hash
     obj = self.new(hash)
     print obj.name
   end
