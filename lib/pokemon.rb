2a5789bc0b8c54d4cb01a193b8305773f011a3df
Joseph Kao <josephtkao@gmail.com>
2020-06-16 00:27:35 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,7 @@ class Pokemon
     SELECT * FROM pokemon
     WHERE id = ?
     SQL
-    row = db.execute(sql, id)
+    row = db.execute(sql, id)[0]
     hash = {:id => row[0], :name => row[1], :type => row[2], :db => db}
     print row
     print hash
